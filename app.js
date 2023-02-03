636a7f89e1bd953694b7b04b1d6889265a662b86
Your Name~ <w3rgy1992@gmail.com>
2023-02-03 18:28:15 +0100

    finish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function imagesInit() {
-	const images = document.querySelector('.article__image');
+	const images = document.querySelectorAll('.article__image');
 	if (images.length) {
 		images.forEach(image => {
 			const imageItem = image.querySelector('img');
@@ -12,8 +12,38 @@ function imagesInit() {
 	}
 }
 
+function gridInit() {
+	const items = document.querySelector('.articles__items');
+	const itemsGrid = new Isotope(items, {
+		itemSelector: '.article',
+		masonry: {
+			fitWidth: true,
+			gutter: 20
+		}
+	});
+
+	document.addEventListener('click', documentActions);
+	function documentActions(e) {
+		const targetElement = e.target;
+		if (targetElement.closest('.filter-articles__item')) {
+			const filterItem = targetElement.closest('.filter-articles__item');
+			const filterValue = filterItem.dataset.filter;
+			const filterActiveItem = document.querySelector('.filter-articles__item.active');
+
+			filterValue === "*" ? itemsGrid.arrange({ filter: `` }) :
+				itemsGrid.arrange({ filter: `[data-filter="${filterValue}"]` })
+
+			filterActiveItem.classList.remove('active');
+			filterItem.classList.add('active');
+
+			e.preventDefault();
+		}
+	}
+}
+
 window.addEventListener('load', windowLoad);
 
 function windowLoad() {
 	imagesInit();
+	gridInit()
 }
